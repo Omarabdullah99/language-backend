b2576072e55d4ba90e29d8c9b76e215b1243e887
Omarabdullah99 <omarabdullah917303@gmail.com>
2024-12-21 21:53:44 +0600

    vocabulary- update, delete,allvoca, vocabyId

--- controllers/vocabularyController.js
+++ controllers/vocabularyController.js
@@ -38,3 +38,52 @@ export const addVocabularyToLesson = async (req, res) => {
       res.status(400).json({ error: error.message });
     }
   };
+
+
+  export const updateVocabulary= async(req,res)=>{
+    try {
+        const {id}= req.params
+        const updateData= req.body
+        const updatedVocabulary = await vocabularyModel.findByIdAndUpdate(
+            id,
+            updateData,
+            { new: true, runValidators: true }
+          );
+          if (!updatedVocabulary) {
+            return res.status(404).json({ error: "Vocabulary not found" });
+          }
+      
+          res.status(200).json(updatedVocabulary);
+        
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+  }
+
+  export const deleteVocabulary = async (req, res) => {
+    try {
+      await vocabularyModel.findByIdAndDelete(req.params.id);
+      res.json({ message: "Vocabulary deleted successfully" });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  };
+
+
+  export const getAllVocabularies = async (req, res) => {
+    try {
+      const vocabularies = await vocabularyModel.find();
+      res.json(vocabularies);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  };
+  
+  export const getVocaById = async (req, res) => {
+    try {
+      const vocabulary = await vocabularyModel.findById(req.params.id);
+      res.json(vocabulary);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  };
--- routes/vocabularyRoute.js
+++ routes/vocabularyRoute.js
@@ -1,10 +1,15 @@
 import express from 'express'
 import authAdmin from '../middlewares/authAdmin.js'
-import { addVocabularyToLesson } from '../controllers/vocabularyController.js'
+import { addVocabularyToLesson, deleteVocabulary, getAllVocabularies, getVocaById, updateVocabulary } from '../controllers/vocabularyController.js'
 
 
 const vocabularyRouter= express.Router()
 
 vocabularyRouter.post('/creat-voca',authAdmin,addVocabularyToLesson)
+vocabularyRouter.patch('/update-voca/:id',authAdmin,updateVocabulary)
+vocabularyRouter.delete('/delete-voca/:id',authAdmin,deleteVocabulary)
+vocabularyRouter.get('/all-voca',getAllVocabularies)
+vocabularyRouter.get('/vocabulary/:id',getVocaById)
+
 
 export default vocabularyRouter
